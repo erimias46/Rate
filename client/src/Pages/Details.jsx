541a9a012718793cd29dae8111d965253f6abe7d
erimias46 <erimias46@gmail.com>
2023-06-19 17:24:40 +0300

    ..

--- client/src/Pages/Details.jsx
+++ client/src/Pages/Details.jsx
@@ -38,34 +38,7 @@ function Details() {
               Depratment :{" "}
               <span className="text-2xl">{teachers.department}</span>
             </p>
-            <div className="rating">
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-orange-400"
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-orange-400"
-                checked
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-orange-400"
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-orange-400"
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-orange-400"
-              />
-            </div>
+            
             <button className="btn btn-primary">Rate</button>
 
             <div>
